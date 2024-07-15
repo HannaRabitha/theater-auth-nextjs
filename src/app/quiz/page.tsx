39b9f3767049f24b9e4582b46be628fdb657b3a8
Hanna <h.rabitha@gmail.com>
2024-07-16 02:12:53 +0700

    Update page.tsx

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,17 +2,35 @@ import React from "react";
 import { db } from "@/lib/db";
 import MateriForm from "@/components/form/MateriForm";
 import QuizForm from "@/components/form/QuizForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
 
 const page = async () => {
 
-    return (
-        <div className="mt-8">
-             <h1 className="text-2xl font-bold text-center leading-snug tracking-tight text-[#2968A3] lg:text-3xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
-             Quiz Section
-            </h1>
+    const session = await getServerSession(authOptions);
 
-            <QuizForm />
-        </div>
+    return (
+
+        <>
+        {session?.user? (
+            <div className="mt-8">
+            <h1 className="text-2xl font-bold text-center leading-snug tracking-tight text-[#2968A3] lg:text-3xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+            Quiz Section
+           </h1>
+
+           <QuizForm />
+       </div>
+        ): (
+            <div className="mt-8">
+            <h1 className="text-2xl font-bold text-center leading-snug tracking-tight text-[#2968A3] lg:text-3xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
+            Quiz Section
+           </h1>
+           <p className="text-center text-xl mt-4">Please login to access this page</p>
+         </div>
+        )}
+        </>
+        
     );
 }
 
